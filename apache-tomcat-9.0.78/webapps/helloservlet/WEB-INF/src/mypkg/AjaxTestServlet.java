10c3d9cd43d25970304d40b26078f6949f1b1b71
LAPTOP-DJOS206A\nikag <nika.gulbit@gmail.com>
2023-07-28 16:43:25 -0400

    remove old sketches

--- apache-tomcat-9.0.78/webapps/helloservlet/WEB-INF/src/mypkg/AjaxTestServlet.java
+++
@@ -1,7 +0,0 @@
-package mypkg;
-
-import java.io.*;
-import java.sql.*;
-import javax.servlet.*;            // Tomcat 9
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
--- apache-tomcat-9.0.78/webapps/helloservlet/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#myAjaxRequestForm").submit(function(e){
-        e.preventDefault();
-    });
-
-    $("#myButton").click(function(e) {
-        
-    });
-});
