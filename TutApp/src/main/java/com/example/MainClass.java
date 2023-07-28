df3ea8eb41d4cf9f2277d1f0ba626cef89c6bd95
LAPTOP-DJOS206A\nikag <nika.gulbit@gmail.com>
2023-07-28 15:35:52 -0400

    Adding Select Tutors

--- TutApp/src/main/java/com/example/MainClass.java
+++ TutApp/src/main/java/com/example/MainClass.java
@@ -1,9 +1,10 @@
 package com.example;
+
 public class MainClass {
-    public static void main(String[] args){
-        RunEngine r = new RunEngine();
-        r.NewUser("Tutor", "jim", "he/him",
-                "abcd@gmail.com", "123@yrdsb.ca", "abcd@gmail.com");
-        r.AssignSubject("jim", "Math", 11);
+    public static void main(String[] args) {
+        String out = RunEngine.searchTutors("Tutor", "Religion", 12);
+        System.out.println(out);
     }
+
 }
+
